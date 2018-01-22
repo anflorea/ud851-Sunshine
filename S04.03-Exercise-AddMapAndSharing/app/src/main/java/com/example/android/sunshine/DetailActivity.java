fb39b5b14fcfd3bb5b5a454e38c6e29afbf1ffe1
Andrei Florea <flo@kuende.com>
2018-01-23 00:59:01 +0200

    Completed exercises from S04

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,11 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -29,6 +33,37 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    // COMPLETED (3) Create a menu with an item with id of action_share
+    // COMPLETED (4) Display the menu and implement the forecast sharing functionality
+
+    private void shareForecastDetails() {
+        String text = mForecast + FORECAST_SHARE_HASHTAG;
+        String mimeType = "text/plain";
+
+        ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setText(text)
+                .startChooser();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.detail, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            shareForecastDetails();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
 }
